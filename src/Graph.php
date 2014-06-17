f93630b8e19555258bd77f541f6a3a3de696d473
Nathanael Tombs <ngctombs@gmail.com>
2014-06-16 22:42:30 -0400

    Implemented Dijsktra's algorithm !w/o trace! in Graph class

--- src/Graph.php
+++ src/Graph.php
@@ -42,8 +42,54 @@ class Graph {
 		}
 	}
 
-	public function getShortestPath($start_node, $end_node) {
-		$shortestPath = 'Implement';
+	/**
+	 * An implementation of Dijkstra's graph traversal algorithm
+	 * @param  string $start_node The node key from which to start
+	 * @param  string $end_node   The node key at which we end
+	 * @return array             The trace of the paths we took
+	 */
+	public function getShortestPath($start_node, $end_node) {		
+		//Instantiate array of node keys to default distance (NULL)
+		foreach ($this->node_list as $key => $value){
+			$dijkstras_nodes[$key] = INF;
+		}
+
+		//Start Node search at the specified start node
+		$current_key = $start_node;
+		$dijkstras_nodes[$current_key] = 0;
+
+		while (isset($dijkstras_nodes[$end_node]) && $current_key != $end_node) {
+			//Symlink because these paths are too damn long!
+			$related_nodes =& $this->node_list[$current_key]->related_nodes;
+
+			//set new weights
+			foreach($related_nodes as $linked_key => $linked_weight) {
+				$new_weight = $dijkstras_nodes[$current_key] + $linked_weight;
+				if (isset($dijkstras_nodes[$linked_key]) && 
+					$dijkstras_nodes[$linked_key] > $new_weight)
+					$dijkstras_nodes[$linked_key] = $new_weight;
+			}
+			unset($dijkstras_nodes[$current_key]);
+
+			//find next lowest weighed node
+			$total_distance = NULL;
+			foreach($dijkstras_nodes as $key => $value) {
+				if($value < $total_distance || is_null($total_distance)) {
+					$current_key = $key;
+					$total_distance = $value;
+				}
+			}
+		}
+		echo "Total Distance = $total_distance";
+
+		$shortestPath = array('Implement');
 		return $shortestPath;
+	}
+
+	/*
+	REMOVE
+	 */
+	public function dirtyDebug($thing) {
+		echo "<pre>".print_r($thing, true)."</pre>";
 	}
 }
